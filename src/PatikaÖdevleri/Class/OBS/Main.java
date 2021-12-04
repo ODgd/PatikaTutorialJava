e1eb6d86da7baa84acf374a1c156ff0c1ad81bf2
ODgd <91457054+ODgd@users.noreply.github.com>
2021-12-04 14:25:58 +0300

    Update Main.java

--- src/PatikaÖdevleri/Class/OBS/Main.java
+++ src/PatikaÖdevleri/Class/OBS/Main.java
@@ -2,25 +2,7 @@ package PatikaÖdevleri.Class.OBS;
 
 
 public class Main {
-
-    //Course Sınıfı Özellikleri :
-    //Nitelikler : name,code,prefix,note,Teacher
-    //Metotlar : Course() , addTeacher() , printTeacher()
-
-    //Teacher Sınıfı Özellikleri :
-    //Nitelikler : name,mpno,branch
-    //Metotlar : Teacher()
-
-    //Student Sınıfı Özellikleri :
-    //Nitelikler : name,stuNo,classes,course1,course2,course3,avarage,isPass
-    //Metotlar : Student(), addBulkExamNote(), isPass(), calcAvarage(), printNote()
-
-    //Ödev
-    //Course sınıfına derse ait sözlü notu kısmını girin ve ortalamaya etkisini her ders için ayrı ayrı belirtin. Sözlü notların ıda ortalamaya etkileme yüzdesi ile dahil edin.
-    //Örnek : Fizik dersine ait sözlü notunun ortalamaya etkisi %20 ise sınav notunun etkisi %80'dir.
-    //Öğrenci sözlüden 90, sınavdan 60 almış ise, o dersin genel ortalamaya etkisi şu şekilde hesaplanır :
-    //Fizik Ortalaması : (90 * 0.20) + (60* 0.80);
-
+    
     public static void main(String[] args) {
 
         Course mat = new Course("Matematik", "101", "MAT");
